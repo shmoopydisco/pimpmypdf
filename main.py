5ceaceda71d6aee28af6b9753eeae79dfe0b3637
shmoopydisco <deadphaker@gmail.com>
2023-08-13 01:31:01 +0300

    make the code work, wrap ocr with cache becuase it would rerun after pressing download

--- main.py
+++ main.py
@@ -2,6 +2,21 @@ import streamlit as st
 import ocrmypdf
 
 
+@st.cache_data
+def run_ocr(input_file, output_file):
+    ocrmypdf.ocr(
+        input_file=input_file,
+        output_file=output_file,
+        deskew=True,
+        clean=True,
+        rotate_pages=True,
+        remove_background=True,
+        # optimize=3,
+        force_ocr=True,
+        progress_bar=False,
+    )
+
+
 def main():
     # streamlit app that recieves a pdf and runs OCRMypdf on it
     st.title("Pimp My PDF")
@@ -10,23 +25,15 @@ def main():
     uploaded_file = st.file_uploader("Choose a file", type="pdf")
     if uploaded_file is not None:
         st.write("Your PDF is being processed. Please wait...")
-        ocrmypdf.ocr(
-            uploaded_file.name,
-            "output.pdf",
-            deskew=True,
-            clean=True,
-            rotate_pages=True,
-            remove_background=True,
-            optimize=3,
-            force_ocr=True,
-        )
+        run_ocr(uploaded_file, "output.pdf")
         st.write("Your PDF has been processed. Please download it below.")
-        st.download_button(
-            label="Download your processed PDF",
-            data="output.pdf",
-            file_name="output.pdf",
-            mime="application/pdf",
-        )
+        with open("output.pdf", "rb") as f:
+            st.download_button(
+                label="Download your processed PDF",
+                data=f,
+                file_name=f"{uploaded_file.name.rstrip('.pdf')}_ocr.pdf",
+                mime="application/pdf",
+            )
 
 
 if __name__ == "__main__":
